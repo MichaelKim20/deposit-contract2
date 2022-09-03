12df9851d12deb3e0412a837752fb2975efb6472
michael.kim <michael.kim.200518@gmail.com>
2022-09-03 15:08:39 +0900

    3

--- staking.sh
+++ staking.sh
@@ -1,5 +1,8 @@
-npx hardhat run scripts/staking1.ts --network devnet
-npx hardhat run scripts/staking2.ts --network devnet
-npx hardhat run scripts/staking3.ts --network devnet
-npx hardhat run scripts/staking4.ts --network devnet
-npx hardhat run scripts/staking5.ts --network devnet
+#npx hardhat run scripts/staking1.ts --network devnet
+#npx hardhat run scripts/staking2.ts --network devnet
+#npx hardhat run scripts/staking3.ts --network devnet
+#npx hardhat run scripts/staking4.ts --network devnet
+#npx hardhat run scripts/staking5.ts --network devnet
+npx hardhat run scripts/staking6.ts --network devnet
+npx hardhat run scripts/staking7.ts --network devnet
+npx hardhat run scripts/staking8.ts --network devnet
