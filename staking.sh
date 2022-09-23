84bef9f515fe6035f999d1c53b358d80dd79b789
michael.kim <michael.kim.200518@gmail.com>
2022-09-23 15:57:04 +0900

    update

--- staking.sh
+++ staking.sh
@@ -1,10 +1,10 @@
-npx hardhat run scripts/staking1.ts --network merge2
-npx hardhat run scripts/staking2.ts --network merge2
-npx hardhat run scripts/staking3.ts --network merge2
-npx hardhat run scripts/staking4.ts --network merge2
-npx hardhat run scripts/staking5.ts --network merge2
-npx hardhat run scripts/staking6.ts --network merge2
-npx hardhat run scripts/staking7.ts --network merge2
+npx hardhat run scripts/staking1.ts --network bellatrix
+npx hardhat run scripts/staking2.ts --network bellatrix
+npx hardhat run scripts/staking3.ts --network bellatrix
+npx hardhat run scripts/staking4.ts --network bellatrix
+npx hardhat run scripts/staking5.ts --network bellatrix
+npx hardhat run scripts/staking6.ts --network bellatrix
+npx hardhat run scripts/staking7.ts --network bellatrix
 
 #npx hardhat run scripts/staking8.ts --network devnet
 #npx hardhat run scripts/staking9.ts --network devnet
